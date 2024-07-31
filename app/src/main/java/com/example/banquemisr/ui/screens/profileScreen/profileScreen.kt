10a42af3c5058898f10a3351629a776655075d53
MohamedAbdELMageid <habibaayman1611@gmail.com>
2024-07-31 16:52:40 -0700

    Profile information screen
    added profile information screen and needed custom components

--- app/src/main/java/com/example/banquemisr/ui/screens/profileScreen/profileScreen.kt
+++ app/src/main/java/com/example/banquemisr/ui/screens/profileScreen/profileScreen.kt
@@ -1,50 +1,109 @@
 package com.example.banquemisr.ui.screens.profileScreen
 
+import android.graphics.Color.parseColor
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.banquemisr.R
-import com.example.banquemisr.ui.screens.reusableUI.profileField.ProfileField
+import com.example.banquemisr.ui.screens.reusableUI.ScreenField
+import com.example.banquemisr.ui.screens.reusableUI.ScreenHeader
 
 @Preview
 @ExperimentalMaterial3Api
 @Composable
 fun ProfileScreen() {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text(text = "Profile") },
-                modifier = Modifier.background(Color.White)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(colors = listOf("#fef8e0".color, "#ffb6c1".color))
             )
-        },
-        content = { _ ->
-            Column(modifier = Modifier.padding(16.dp)) {
-                ProfileOptions()
-            }
-        }
-    )
+    ) {
+        ProfileScreenHeader()
+        ProfileName()
+        ProfileOptions()
+    }
+
 }
 
 @Composable
 fun ProfileOptions() {
-    Column {
-        ProfileField("Personal information", "Your information", R.drawable.user)
-        HorizontalDivider(thickness = 2.dp, color = Color.Black)
-        ProfileField("Setting", "Change your settings", R.drawable.setting)
-        HorizontalDivider(thickness = 2.dp, color = Color.Black)
-        ProfileField("Payment history", "View your transactions", R.drawable.bank_account)
-        HorizontalDivider(thickness = 2.dp, color = Color.Black)
-        ProfileField("My favourite list", "View your favourites", R.drawable.star)
-        HorizontalDivider(thickness = 2.dp, color = Color.Black)
+    Column(modifier = Modifier.padding(top = 8.dp)) {
+        ScreenField("Personal information", "Your information", R.drawable.user)
+        HorizontalDivider(
+            thickness = 1.dp,
+            color = Color.Gray,
+            modifier = Modifier.padding(horizontal = 10.dp)
+        )
+        ScreenField("Setting", "Change your settings", R.drawable.setting)
+        HorizontalDivider(
+            thickness = 1.dp,
+            color = Color.Gray,
+            modifier = Modifier.padding(horizontal = 10.dp)
+        )
+        ScreenField("Payment history", "View your transactions", R.drawable.bank_account)
+        HorizontalDivider(
+            thickness = 1.dp,
+            color = Color.Gray,
+            modifier = Modifier.padding(horizontal = 10.dp)
+        )
+        ScreenField("My favourite list", "View your favourites", R.drawable.star)
+        HorizontalDivider(
+            thickness = 1.dp,
+            color = Color.Gray,
+            modifier = Modifier.padding(horizontal = 10.dp)
+        )
     }
 }
+
+@Composable
+fun ProfileScreenHeader() {
+    ScreenHeader("Profile", onClick = {})
+}
+
+@Composable
+fun ProfileName() {
+    Row(
+        modifier = Modifier
+            //.padding(vertical = 8.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(5.dp)
+                .clip(RoundedCornerShape(50.dp))
+                .background(color = Color(0xFFccced3))
+                .size(60.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(text = "AD", fontWeight = FontWeight.Bold, fontSize = 20.sp, color = Color.Gray)
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(text = "Asmaa Dosuky",fontWeight = FontWeight.Bold, fontSize = 24.sp, color = Color.Black)
+    }
+}
+
+val String.color
+    get() = Color(parseColor(this))
--- app/src/main/java/com/example/banquemisr/ui/screens/reusableUI/screenField.kt
+++ app/src/main/java/com/example/banquemisr/ui/screens/reusableUI/screenField.kt
@@ -1,16 +1,24 @@
-package com.example.banquemisr.ui.screens.reusableUI.profileField
+package com.example.banquemisr.ui.screens.reusableUI
 
+import android.graphics.Color.parseColor
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -18,10 +26,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.banquemisr.R
+import com.example.banquemisr.ui.screens.profileScreen.color
 
 @Preview
 @Composable
-fun ProfileField(
+fun ScreenField(
     headLine: String = "",
     supportingText: String = "",
     leadingIcon: Int = 0,
@@ -30,18 +39,28 @@ fun ProfileField(
     Row(
         modifier = Modifier
             .padding(vertical = 8.dp)
-        .fillMaxWidth()
+            .fillMaxWidth()
         //.clickable {  }
         ,
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Row {
-            Icon(
-                painter = painterResource(id = leadingIcon),
-                contentDescription = "",
-                modifier = Modifier.size(50.dp)
-            )
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Box(
+                modifier = Modifier
+                    .padding(5.dp)
+                    .clip(RoundedCornerShape(20.dp))
+                    .background(color = Color(0xFFccced3))
+                    .size(60.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    painter = painterResource(id = leadingIcon),
+                    contentDescription = "",
+                    modifier = Modifier.size(40.dp)
+                )
+            }
+            Spacer(modifier = Modifier.width(5.dp))
             Column {
                 Text(text = headLine, fontWeight = FontWeight.Bold, fontSize = 16.sp)
                 Text(text = supportingText, color = Color.Gray, fontSize = 14.sp)
@@ -55,3 +74,4 @@ fun ProfileField(
 
     }
 }
+
