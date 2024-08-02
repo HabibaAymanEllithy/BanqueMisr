9d77ca9026da9cff8fccc5c738f16f706a7caeaf
MohamedAbdELMageid <habibaayman1611@gmail.com>
2024-08-02 11:46:34 -0700

    solving conflict

--- app/src/main/java/com/example/banquemisr/MainActivity.kt
+++ app/src/main/java/com/example/banquemisr/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.banquemisr.ui.theme.BanqueMisrTheme
 
@@ -16,17 +19,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BanqueMisrTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { _ ->
 
+                }
             }
         }
     }
-}
 
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BanqueMisrTheme {
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        BanqueMisrTheme {
 
-    }
-}
+        }
+    }}
