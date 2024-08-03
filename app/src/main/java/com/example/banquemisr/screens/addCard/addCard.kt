d2f3fc7d07b08fc1db4adeca224c135f544209c6
malak el wassif <102263929+malakElWassif@users.noreply.github.com>
2024-08-03 17:18:22 +0300

    add Card

--- app/src/main/java/com/example/banquemisr/screens/addCard/addCard.kt
+++ app/src/main/java/com/example/banquemisr/screens/addCard/addCard.kt
@@ -1,10 +1,18 @@
 package com.example.banquemisr.screens.addCard
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -15,13 +23,16 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -62,7 +73,7 @@ fun addCardScreen(navController: NavController, modifier: Modifier = Modifier) {
                         contentAlignment = Alignment.Center
                     ) {
                         Text(
-                            "Select Currency",
+                            "Add Card",
                             fontSize = 20.sp,
                             textAlign = TextAlign.Center,
                             fontWeight = FontWeight.W500
@@ -90,17 +101,48 @@ fun addCardScreen(navController: NavController, modifier: Modifier = Modifier) {
         }
     ) { innerPadding ->
         addCard(
-            navController,cardHolderName.value,cardNO.value,CVV.value,expiryDate.value, innerPadding
+            navController,cardHolderName,cardNO,CVV,expiryDate, innerPadding
         )
     }
 }
 
 @Composable
-fun addCard(navController: NavController,cardHolder:String,CardNo:String,CVV:String,expiryDate:String,paddingValues: PaddingValues){
-TextFields(string1 = "cardHolder Name", string2 = "Enter cardHolder Name", icon =R.drawable.transparent_icon , state =cardHolder. , keyboard = , isPassword = )
+fun addCard(navController: NavController, cardHolder:MutableState<String>, CardNo: MutableState<String>, CVV:MutableState<String>, expiryDate:MutableState<String>, innerPadding: PaddingValues,modifier: Modifier=Modifier){
+    val scrollState = rememberScrollState()
+    var background = Brush.verticalGradient(
+        listOf(colorResource(id = R.color.Greadient2), colorResource(id = R.color.Gredient)),
+        startY = 2000f,
+        endY = 0f
+    )
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxSize()
+
+            .background(background)
+            .verticalScroll(scrollState).padding(innerPadding)
+    ) {
 
 
+        TextFields(
+            string1 = "cardHolder Name",
+            string2 = "Enter cardHolder Name",
+            icon = R.drawable.transparent,
+            state = cardHolder,
+            keyboard = KeyboardOptions(keyboardType = KeyboardType.Text),
+            isPassword = false
+        )
+        TextFields(
+            string1 = "Card NO",
+            string2 = "Card NO",
+            icon = R.drawable.transparent_icon,
+            state = cardHolder,
+            keyboard = KeyboardOptions(keyboardType = KeyboardType.Decimal),
+            isPassword = false
+        )
 
+
+    }
 
 
 
